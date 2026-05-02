d53f52ef468fbc134bef6fefd69fe854b92c773a
Yomafil <128907736+Yomafil@users.noreply.github.com>
2026-05-02 03:42:51 +0200

    Init PoC add epub publisher metadata

--- plugin/js/parsers/Template.js
+++ plugin/js/parsers/Template.js
@@ -136,6 +136,14 @@ class TemplateParser extends Parser { // eslint-disable-line no-unused-vars
     }
     */
 
+    // Publisher of the story
+    // Optional, Publisher for metadata, if not provided, will default to ""
+    /*
+    extractPublisher(dom) {
+        return "Site Name";
+    }
+    */
+
     // Optional, supply if need to do special manipulation of content
     // e.g. decrypt content
     /*
